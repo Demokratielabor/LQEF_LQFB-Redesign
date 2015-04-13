0c517a05cdff8dcbcbbbaae3ea717446dde3f33f
Thomas Sänger <HorayNarea@gmail.com>
2015-04-13 10:35:16 +0200

    wrapper-div for events

--- app/main/event/_list.lua
+++ app/main/event/_list.lua
@@ -63,136 +63,138 @@ filters.content = function()
     for i, event in ipairs(events) do
       last_event_id = event.id
 
-      ui.container{ attr = { class = "event_info" }, content = function()
-        local event_name = event.event_name
-        local event_image
-        if event.event == "issue_state_changed" then
-          if event.state == "discussion" then
-            event_name = _"Discussion started"
-            event_image = "comments.png"
-          elseif event.state == "verification" then
-            event_name = _"Verification started"
-            event_image = "lock.png"
-          elseif event.state == "voting" then
-            event_name = _"Voting started"
-            event_image = "email_open.png"
-          elseif event.state == "finished_with_winner" then
-            event_name = event.state_name
-            event_image = "award_star_gold_2.png"
-          elseif event.state == "finished_without_winner" then
-            event_name = event.state_name
-            event_image = "cross.png"
+      ui.container{ attr = { class = "eventblock" }, content = function()
+        ui.container{ attr = { class = "event_info" }, content = function()
+          local event_name = event.event_name
+          local event_image
+          if event.event == "issue_state_changed" then
+            if event.state == "discussion" then
+              event_name = _"Discussion started"
+              event_image = "comments.png"
+            elseif event.state == "verification" then
+              event_name = _"Verification started"
+              event_image = "lock.png"
+            elseif event.state == "voting" then
+              event_name = _"Voting started"
+              event_image = "email_open.png"
+            elseif event.state == "finished_with_winner" then
+              event_name = event.state_name
+              event_image = "award_star_gold_2.png"
+            elseif event.state == "finished_without_winner" then
+              event_name = event.state_name
+              event_image = "cross.png"
+            else
+              event_name = event.state_name
+            end
+            if event_image then
+              ui.image{ static = "icons/16/" .. event_image }
+              slot.put(" ")
+            end
+          end
+          local days_ago_text
+          if event.time_ago == 0 then
+            days_ago_text = _("Today at #{time}", { time = format.time(event.occurrence) })
+          elseif event.time_ago == 1 then
+            days_ago_text = _("Yesterday at #{time}", { time = format.time(event.occurrence) })
           else
-            event_name = event.state_name
+            days_ago_text = _("#{date} at #{time}", { date = format.date(event.occurrence.date), time = format.time(event.occurrence, { hide_seconds = true }) })
           end
-          if event_image then
-            ui.image{ static = "icons/16/" .. event_image }
-            slot.put(" ")
+          ui.tag{ attr = { class = "event_name" }, content = event_name }
+          slot.put("<br />") 
+          ui.tag{ content = days_ago_text }
+    --[[      if event.time_ago > 1 then
+            slot.put("<br />(")
+            ui.tag{ content = _("#{count} days ago", { count = event.time_ago }) }
+            slot.put(")")
           end
-        end
-        local days_ago_text
-        if event.time_ago == 0 then
-          days_ago_text = _("Today at #{time}", { time = format.time(event.occurrence) })
-        elseif event.time_ago == 1 then
-          days_ago_text = _("Yesterday at #{time}", { time = format.time(event.occurrence) })
-        else
-          days_ago_text = _("#{date} at #{time}", { date = format.date(event.occurrence.date), time = format.time(event.occurrence, { hide_seconds = true }) })
-        end
-        ui.tag{ attr = { class = "event_name" }, content = event_name }
-        slot.put("<br />") 
-        ui.tag{ content = days_ago_text }
-  --[[      if event.time_ago > 1 then
-          slot.put("<br />(")
-          ui.tag{ content = _("#{count} days ago", { count = event.time_ago }) }
-          slot.put(")")
-        end
-        --]]
-        if app.session:has_access("authors_pseudonymous") and event.member_id then
-          slot.put("<br />") 
-          slot.put("<br />") 
-          if app.session.member_id then
-            ui.link{
-              content = function()
-                execute.view{
-                  module = "member_image",
-                  view = "_show",
-                  params = {
-                    member = event.member,
-                    image_type = "avatar",
-                    show_dummy = true,
-                    class = "micro_avatar",
-                    popup_text = text
+          --]]
+          if app.session:has_access("authors_pseudonymous") and event.member_id then
+            slot.put("<br />") 
+            slot.put("<br />") 
+            if app.session.member_id then
+              ui.link{
+                content = function()
+                  execute.view{
+                    module = "member_image",
+                    view = "_show",
+                    params = {
+                      member = event.member,
+                      image_type = "avatar",
+                      show_dummy = true,
+                      class = "micro_avatar",
+                      popup_text = text
+                    }
                   }
-                }
-              end,
+                end,
+                module = "member", view = "show", id = event.member_id
+              }
+              slot.put(" ")
+            end
+            ui.link{
+              text = event.member.name,
               module = "member", view = "show", id = event.member_id
             }
-            slot.put(" ")
-          end
-          ui.link{
-            text = event.member.name,
-            module = "member", view = "show", id = event.member_id
-          }
-        end
-      end }
-
-      ui.container{ attr = { class = "issue" }, content = function()
-
-        execute.view{ module = "delegation", view = "_info", params = { issue = event.issue, member = for_member } }
-
-        ui.container{ attr = { class = "content" }, content = function()
-          ui.link{
-            module = "unit", view = "show", id = event.issue.area.unit_id,
-            attr = { class = "unit_link" }, text = _("unit") .. ": " .. event.issue.area.unit.name
-          }
-          slot.put(" ")
-          ui.link{
-            module = "area", view = "show", id = event.issue.area_id,
-            attr = { class = "area_link" }, text = _("area") .. ": " .. event.issue.area.name
-          }
-        end }
-        
-        ui.container{ attr = { class = "title" }, content = function()
-          ui.link{
-            attr = { class = "issue_id" },
-            text = _("#{policy} ##{id}", { policy = event.issue.policy.name, id = event.issue_id }),
-            module = "issue",
-            view = "show",
-            id = event.issue_id
-          }
-        end }
-
-        ui.container{ attr = { class = "initiative_list" }, content = function()
-          if not event.initiative_id then
-            local initiatives_selector = Initiative:new_selector()
-              :add_where{ "initiative.issue_id = ?", event.issue_id }
-              :add_order_by("initiative.admitted DESC NULLS LAST, initiative.rank NULLS LAST, initiative.harmonic_weight DESC NULLS LAST, id")
-            execute.view{ module = "initiative", view = "_list", params = { 
-              issue = event.issue,
-              initiatives_selector = initiatives_selector,
-              no_sort = true,
-              limit = 5,
-              for_member = for_member
-            } }
-          else
-            local initiatives_selector = Initiative:new_selector()
-              :add_where{ "initiative.id = ?", event.initiative_id }
-            execute.view{ module = "initiative", view = "_list", params = { 
-              initiatives_selector = initiatives_selector,
-              no_sort = true,
-              hide_more_initiatives = true,
-              for_member = for_member
-            } }
           end
         end }
 
-        ui.container{ attr = { class = "content suggestion" }, content = function()
-          if event.suggestion_id then
+        ui.container{ attr = { class = "issue" }, content = function()
+
+          execute.view{ module = "delegation", view = "_info", params = { issue = event.issue, member = for_member } }
+
+          ui.container{ attr = { class = "content" }, content = function()
             ui.link{
-              text = event.suggestion.name,
-              module = "suggestion", view = "show", id = event.suggestion_id
+              module = "unit", view = "show", id = event.issue.area.unit_id,
+              attr = { class = "unit_link" }, text = _("unit") .. ": " .. event.issue.area.unit.name
             }
-          end
+            slot.put(" ")
+            ui.link{
+              module = "area", view = "show", id = event.issue.area_id,
+              attr = { class = "area_link" }, text = _("area") .. ": " .. event.issue.area.name
+            }
+          end }
+          
+          ui.container{ attr = { class = "title" }, content = function()
+            ui.link{
+              attr = { class = "issue_id" },
+              text = _("#{policy} ##{id}", { policy = event.issue.policy.name, id = event.issue_id }),
+              module = "issue",
+              view = "show",
+              id = event.issue_id
+            }
+          end }
+
+          ui.container{ attr = { class = "initiative_list" }, content = function()
+            if not event.initiative_id then
+              local initiatives_selector = Initiative:new_selector()
+                :add_where{ "initiative.issue_id = ?", event.issue_id }
+                :add_order_by("initiative.admitted DESC NULLS LAST, initiative.rank NULLS LAST, initiative.harmonic_weight DESC NULLS LAST, id")
+              execute.view{ module = "initiative", view = "_list", params = { 
+                issue = event.issue,
+                initiatives_selector = initiatives_selector,
+                no_sort = true,
+                limit = 5,
+                for_member = for_member
+              } }
+            else
+              local initiatives_selector = Initiative:new_selector()
+                :add_where{ "initiative.id = ?", event.initiative_id }
+              execute.view{ module = "initiative", view = "_list", params = { 
+                initiatives_selector = initiatives_selector,
+                no_sort = true,
+                hide_more_initiatives = true,
+                for_member = for_member
+              } }
+            end
+          end }
+
+          ui.container{ attr = { class = "content suggestion" }, content = function()
+            if event.suggestion_id then
+              ui.link{
+                text = event.suggestion.name,
+                module = "suggestion", view = "show", id = event.suggestion_id
+              }
+            end
+          end }
         end }
       end }
     end
